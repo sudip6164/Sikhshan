86c65b704630d71e5978f8ed7871d1352b443f7b
sudip6164 <sudippradhanadgj@gmail.com>
2025-05-19 09:04:44 +0545

    login api

--- src/main/java/com/sikhshan/repository/UserRepository.java
+++ src/main/java/com/sikhshan/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.sikhshan.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.sikhshan.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-	
+	Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/sikhshan/restcontroller/UserController.java
+++ src/main/java/com/sikhshan/restcontroller/UserController.java
@@ -1,18 +1,19 @@
 package com.sikhshan.restcontroller;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sikhshan.model.Role;
+import com.sikhshan.dto.LoginRequest;
+import com.sikhshan.dto.LoginResponse;
 import com.sikhshan.model.User;
 import com.sikhshan.repository.UserRepository;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,4 +21,40 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	}
+	@PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+	
+	 @PostMapping("/login")
+	    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+	        Optional<User> optionalUser = userRepository.findByEmail(loginRequest.getEmail());
+
+	        if (optionalUser.isPresent()) {
+	            User user = optionalUser.get();
+
+	            if (user.getPassword().equals(loginRequest.getPassword()) && user.getRole() == loginRequest.getRole()) {
+	                String message;
+	                switch (user.getRole()) {
+	                    case Student:
+	                        message = "Welcome to Student Dashboard!";
+	                        break;
+	                    case Faculty:
+	                        message = "Welcome to Faculty Dashboard!";
+	                        break;
+	                    case Admin:
+	                        message = "Welcome to Admin Panel!";
+	                        break;
+	                    default:
+	                        message = "Welcome!";
+	                }
+	                return ResponseEntity.ok(new LoginResponse(message));
+	            } else {
+	                return ResponseEntity.status(401).body("Invalid credentials or role");
+	            }
+	        } else {
+	            return ResponseEntity.status(401).body("User not found");
+	        }
+	    }
+}
