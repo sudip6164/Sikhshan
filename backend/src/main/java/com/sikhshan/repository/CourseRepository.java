1ba9b438100758dfc964c44a7918454b3e28c5cf
sudip6164 <sudippradhanadgj@gmail.com>
2025-07-20 14:35:46 +0545

    Resolve merge: keep frontend deletions for backend files

--- backend/src/main/java/com/sikhshan/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sikhshan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sikhshan.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
